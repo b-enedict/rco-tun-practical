7a6f53207bc3a53e22965db38079514e8ea361ac
b-enedict <benedict.seuss@gmail.com>
2025-11-14 19:02:24 +0100

    fix: fix size reading bugs

--- cipher_test.c
+++ cipher_test.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "cesar.h"
+
+#define BUFSIZE 200
 
 int main() {
-    char *buffer = malloc(200);
+    char *buffer = malloc(BUFSIZE);
 
     while(1) {
-        printf("Enter your text: ");
-        fgets(buffer, sizeof(buffer), stdin);
+        printf("Enter your text (or press Enter to quit): ");
+
+        if (!fgets(buffer, BUFSIZE, stdin)) {
+            // EOF or error
+            break;
+        }
+
+        // If user just presses Enter, quit
+        if (buffer[0] == '\n' || buffer[0] == '\0') {
+            break;
+        }
         
-        int plen = strlen(buffer);
-        char *temp = malloc(plen);
+        int plen = (int) strlen(buffer);
+        char *temp = malloc(plen + 1);
         
         // Encryption Logic
         printf("\nEncrypting a packet of length: %d", plen);
-        memcpy(temp, buffer, plen);
+        cesar_encrypt(buffer, plen, temp);
+        temp[plen] = '\0';
 
         // Output the encrypted packet
         printf(", Encrypted packet:\n\n%s\n", temp);
 
         // Decryption Logic
-        memcpy(buffer, temp, plen);
+        cesar_decrypt(temp, plen, buffer);
+        buffer[plen] = '\0';
 
         printf("Decrypted packet:\n\n%s\n\n", buffer);
         free(temp);
